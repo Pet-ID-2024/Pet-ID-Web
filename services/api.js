55015493cb3b3a5f990b30caa90c9dba091c84a8
JJM <ideal3339@gmail.com>
2025-01-06 16:10:00 +0900

    add : sendContentNoti, env fix

--- services/api.js
+++ services/api.js
@@ -11,6 +11,7 @@ const updateBanner = (id, bannerData) => api.put(`/v1/banner/${id}`, bannerData,
 const fetchBookingList = (status) => api.get('/v1/manage/hospital/order',{params : {status : status}});
 const sendBookingNoti = (userData) => api.post('/v1/notification/booking',userData);
 
+
 const fetchContentList = (category) => api.get('/v1/content',{params : {isFullBody : true, category : category}});
 export const getContentPresignedUrl = (filePath) => api.post('/v1/content/presigned-put-url', filePath);
 export const fetchContentImgs = (filePath) => api.get('/v1/content/presigned-get-url', {
@@ -19,6 +20,7 @@ export const fetchContentImgs = (filePath) => api.get('/v1/content/presigned-get
 export const updateContent = (contentId, contentData) => api.put(`/v1/content/${contentId}`, contentData );
 export const createContent = (contentData) => api.post('/v1/content', contentData);
 export const deleteContent = (contentId) => api.delete(`/v1/content${contentId}`);
+export const sendContentNoti = (fcm) => api.post('/v1/notification/content',fcm);
 
 
 export { saveBanner, getPresignedUrl, uploadImage, fetchBanners, updateBanner, fetchBannerImgs, fetchBookingList, sendBookingNoti , fetchContentList};
