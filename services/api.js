00cb53d8ab099fa192efd86bcfb40eb548a776c9
JJM <ideal3339@gmail.com>
2025-01-23 16:23:02 +0900

    add : delete banner api

--- services/api.js
+++ services/api.js
@@ -7,6 +7,7 @@ const uploadImage = (url, image) => axios.put(url, image, { headers: { 'Content-
 const fetchBanners = (type) => api.get('/v1/banner/type',{params : {type : type},});
 const fetchBannerImgs = (filePath) => api.get('/v1/banner/presigned-put-url', filePath,);
 const updateBanner = (id, bannerData) => api.put(`/v1/banner/${id}`, bannerData, );
+const deleteBanner = (id) => api.delete(`/v1/banner/${id}`);
 
 
 const fetchBookingList = (status) => api.get('/v1/manage/hospital/order',{params : {status : status}});
@@ -20,10 +21,10 @@ export const fetchContentImgs = (filePath) => api.get('/v1/content/presigned-get
   });
 export const updateContent = (contentId, contentData) => api.put(`/v1/content/${contentId}`, contentData );
 export const createContent = (contentData) => api.post('/v1/content', contentData);
-export const deleteContent = (contentId) => api.delete(`/v1/content${contentId}`);
+export const deleteContent = (contentId) => api.delete(`/v1/content/${contentId}`);
 export const sendContentNoti = (fcm) => api.post('/v1/notification/content',fcm);
 
 
-export { saveBanner, getPresignedUrl, uploadImage, fetchBanners, updateBanner, fetchBannerImgs, fetchBookingList, sendBookingNoti , fetchContentList};
+export { saveBanner, getPresignedUrl, uploadImage, fetchBanners, updateBanner, fetchBannerImgs, fetchBookingList, sendBookingNoti , fetchContentList, deleteBanner};
 
   
